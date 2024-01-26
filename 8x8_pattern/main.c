85ba13d91aab19bf3b56196e40bc58287a430f1c
VK-Monish <139607720+VK-Monish@users.noreply.github.com>
2024-01-26 16:07:56 +0530

    Update main.c

--- 8x8_pattern/main.c
+++ 8x8_pattern/main.c
@@ -1,3 +1,8 @@
+/******************************************************/
+/*   NAME        : MONISH KUMAR.K                     */
+/*   DATE        : 26/01/2024                         */
+/*   DESCRIPTION : 8X8 PATTERN                        */
+/******************************************************/
 volatile unsigned int *RCC_CR      = (volatile unsigned int *)0x40023800;
 volatile unsigned int *RCC_CFGR    = (volatile unsigned int *)0x40023808;
 volatile unsigned int *RCC_AHB1ENR = (volatile unsigned int *)0x40023830;
